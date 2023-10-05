c8e221b8b2c9239856362cf8c516dceed8af5990
samuel <samuka1999cristovao@gmail.com>
2023-10-05 19:47:02 -0300

    inicio do react

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+return(
+  <Container>
+    <Profile>
+      <img src="https://github.com/samuelc99.png" alt="Foto do usuário" />
+
+      <div>
+        <span>Bem-vindo</span>
+        <strong>Samuel Cristóvão</strong>
+      </div>
+    </Profile>
+
+    <Logout>
+      <RiShutDownLine/>
+    </Logout>
+
+  </Container>
+)
+}
+
